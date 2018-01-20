c216c54ac0c12c6fc82b60fc28156962057b3d2b
YuyaOaku <32827194+y-ascll@users.noreply.github.com>
2018-01-20 23:40:01 +0900

    Update kadai2.m

--- kadai2.m
+++ kadai2.m
@@ -26,9 +26,12 @@ pause;
 % ８階調画像の生成
 IMG0 = ORG>32;
 IMG1 = ORG>64;
-IMG2 = ORG>128;
-IMG3 = ORG>192;
-IMG4 = ORG>224;
-IMG = IMG0 + IMG1 + IMG2 + IMG3 + IMG4;
+IMG2 = ORG>96;
+IMG3 = ORG>128;
+IMG4 = ORG>160;
+IMG5 = ORG>192;
+IMG6 = ORG>224;
+IMG7 = ORG>256;
+IMG = IMG0 + IMG1 + IMG2 + IMG3 + IMG4 + IMG5 + IMG6 + IMG7;
 imagesc(IMG); colormap(gray); colorbar;  axis image;
 pause;
